524fc674fc2c7fc2116dedce8e396372c85cc591
Rik Vandenheede <Rik.Vandenheede0@hotmail.com>
2021-10-19 00:08:41 +0200

    les7 complete

--- Sectie-7-web-servers/src/app.js
+++ Sectie-7-web-servers/src/app.js
@@ -1,8 +1,23 @@
+const path = require("path"); // core node module
 const express = require("express"); // express is een functie, gebruikt om een applicatie aan te maken
+const hbs = require("hbs");
+
+// nodemon src/app.js -e js,hbs // laat weten dat deze ook moeten geupdate worden door de e flag
+
+console.log(__dirname); // geeft de route van de map waar deze file in zit terug
+console.log(path.join(__dirname, "../public")); // geeft het path naar de public map // kijk lijn 10
 
 const app = express(); // genereert een express app, om de server te configureren passen we methodes toe op de app
-
 // hieronder zeggen wat onze express app moet doen
+const publicDirPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials"); // laat handlebars weten waar hij moet zoeken, lijn 17
+
+app.use(express.static(publicDirPath));// cinfiguered express app
+app.set("views", viewsPath);
+app.set("view engine", "hbs"); // laat express weten welke template engine we gebruiken
+hbs.registerPartials(partialsPath);
+
 
 // app.get() // laat ons configureren wat de server moet doen als iemand naarhier surft
 // neemt 2 argumenten 1(de route) 2(en een functie)
@@ -10,24 +25,46 @@ const app = express(); // genereert een express app, om de server te configurere
 // 2 een functie => zegt wat we willen doen wanneer iemand deze specifieke route bezoekt
 
 app.get("", (req, res) => { // req => informatie van inkomende informatie // res => een heleboel methodes, waardoor we kunnen kiezen wat we terugsturen naar de requester
-    res.send("Hello Express"); // => zend iets terug naar de persoon die de roete bezoekt
+    //res.send("Hello Express"); // => zend iets terug naar de persoon die de roete bezoekt
+    res.render("index", {// moet matchen met de naam in de views folder, door de hbs engine weet express dat hij daar moet kijken
+        title: "Weather app", // in de index kan je nu {{title}} gebruiken
+        name: "Rik"
+    }); 
 })
 
 app.get("/help", (req, res) => {
-    res.send({ // stuur een object of array mee, deze wordt als JSON meegegeven aan de requester
-        name: "Rik", // als je deze route opzoekt dan zie je geformat json die je kan gebruiken
-        age: 22
-    });
+    res.render("help", {
+        title: "Help",
+        message: "Dit is de help message :)",
+        name: "Rik"
+    })
 })
 
 app.get("/about", (req, res) => {
-    res.send("<h1>About page</h1>") // je kan er html aan meegeven
+    res.render("about", {
+        title: "About page",
+        name: "Rik"
+    })
 })
 
 app.get("/weather", (req, res) => {
     res.send({
         location: "Binkom",
         degrees: 12
+    })
+})
+
+// deze matcht met geen enkele pagina hierboven maar wel met een foute /help/...
+app.get("/help/*", (req, res) => {
+    res.render("404", {
+        message: "Help article not found"
+    });
+})
+
+// deze moet als laatste omdat express voor een "match zoekt" en begint vanboven
+app.get("*", (req, res) => {
+    res.render("404", {
+        message: "My 404 page"
     })
 })
 
